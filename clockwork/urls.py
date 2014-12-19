43ef6886ebdea7296afb15e1af3d0f631a1cc21f
Jon Simington <jon.simington@mst.edu>
2014-12-19 08:20:29 +0000

    Add base home app

--- clockwork/urls.py
+++ clockwork/urls.py
@@ -2,9 +2,11 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'clockwork.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
+
+    # home app
+    url(r'^', include('clockwork.home.urls'),
+
+    # user profiles
+    url(r'^', include('clockwork.profiles.urls'),
 )
