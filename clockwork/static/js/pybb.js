ed4b97835b78954a071f785e03c339937817d371
Jon Simington <jon.simington@mst.edu>
2015-01-08 17:39:41 -0600

    Show BBCode tags on post form

--- clockwork/static/js/pybb.js
+++ clockwork/static/js/pybb.js
@@ -12,7 +12,68 @@ function quote_selected(user) {
     document.getElementById("id_body").focus()
 }
 
-function add_color_tag() {
-    document.getElementById("id_body").value += "[color=" + '"<color>"'+ "]your_text_here[/color]"
+function addColor() {                                               
+    document.getElementById("id_body").value += "[color=][/color]"
     document.getElementById("id_body").focus()
-}
+}  
+
+function addBold() {                                               
+    document.getElementById("id_body").value += "[b][/b]"
+    document.getElementById("id_body").focus()
+}  
+
+function addItalics() {                                               
+    document.getElementById("id_body").value += "[i][/i]"
+    document.getElementById("id_body").focus()
+}  
+
+function addUnderline() {                                               
+    document.getElementById("id_body").value += "[u][/u]"
+    document.getElementById("id_body").focus()
+}  
+
+function addStrikethrough() {                                               
+    document.getElementById("id_body").value += "[s][/s]"
+    document.getElementById("id_body").focus()
+}  
+
+function addHR() {                                               
+    document.getElementById("id_body").value += "[hr]"
+    document.getElementById("id_body").focus()
+}  
+
+function addSubscript() {                                               
+    document.getElementById("id_body").value += "<base>[sub]<subscript>[/sub]"
+    document.getElementById("id_body").focus()
+}  
+
+function addSuperscript() {                                               
+    document.getElementById("id_body").value += "<base>[sup]<superscript>[/sup]"
+    document.getElementById("id_body").focus()
+}  
+
+function addList() {                                               
+    document.getElementById("id_body").value += "[list][*]<item 1>[*]<item 2>[/list]"
+    document.getElementById("id_body").focus()
+}  
+
+function addQuote() {                                               
+    document.getElementById("id_body").value += "[quote][/quote]"
+    document.getElementById("id_body").focus()
+}  
+
+function addCode() {                                               
+    document.getElementById("id_body").value += "[code][/code]"
+    document.getElementById("id_body").focus()
+}  
+
+function addCenter() {                                               
+    document.getElementById("id_body").value += "[center][/center]"
+    document.getElementById("id_body").focus()
+}  
+
+function addURL() {                                               
+    document.getElementById("id_body").value += "[url=<url>]<link text>[/url]"
+    document.getElementById("id_body").focus()
+}  
+
