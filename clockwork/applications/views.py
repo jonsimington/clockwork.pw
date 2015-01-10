9eaf7bb969c963a5f1b374dc01b2d736a8f7acb0
jswv9@mst.edu <jswv9@mst.edu>
2015-01-09 19:21:41 -0600

    Remove applications app

--- clockwork/applications/views.py
+++
@@ -1,7 +0,0 @@
-from django.views.generic.detail import DetailView
-
-from .forms import ApplicationForm
-from .models import UserProfile
-
-def ApplicationFormView(request):
-    return render(request, 'app_form.html', {'form': ApplicationForm()})
